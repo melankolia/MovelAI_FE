7803719edbd8ea9a8c0139569af8b682890cbaba
Ageng Setyo Nugroho <setyo.nugroho96@gmail.com>
2021-05-20 13:08:00 +0700

    Adding .env

--- src/services/main.instance.js
+++ src/services/main.instance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+
 const instance = axios.create({
-    baseURL: "http://localhost:9000"
+    baseURL: process.env.REACT_APP_BASE_URL
 });
 
 instance.interceptors.request.use((config) => {
